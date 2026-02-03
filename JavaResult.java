0eddaf24d9ca7b907628b7097712d01cc81da84e
adityaprakash8419 <adityaprakash8419@gmail.com>
2026-02-03 16:09:17 +0530

    [Aditya] created ArrayTraverse.java GreatestElement.java and LeetCode1480.java

---
+++ JavaResult.java
@@ -0,0 +1,16 @@
+class JavaResult{
+	int rollno = 101;
+		String name = "Raj";
+		double  percentage=85.10;
+		char result = 'P';
+	public  static void main (String[] args){
+		Main obj = new Main();
+
+		//jd = new JavaResult();
+		System.out.println(obj.rollno);
+		System.out.println(obj.name);
+		System.out.println(obj.percentage);
+		System.out.println(obj.result);
+		
+	}
+}
